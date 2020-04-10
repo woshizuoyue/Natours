73c22a045e68869582c39cdddc5ca3ae686ea203
Yue Zuo <woshizuoyue9201@yahoo.com>
2020-04-10 11:32:17 -0400

    create middleware

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const fs = require('fs');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log('Hello from the middleware...');
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
