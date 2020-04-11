6bc44ebe35d9307271d320722a08bd04ab60d71d
Yue Zuo <woshizuoyue9201@yahoo.com>
2020-04-11 11:16:15 -0400

    create user router

--- app.js
+++ app.js
@@ -79,14 +79,52 @@ const updateTour = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
-      tour: '<updated tour here...',
+      tour: 'updated tour here...',
     },
+  });
+};
+
+const getAllUsers = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const getUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const updateUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+const deleteUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
   });
 };
 
 //3. routers
 app.route('/api/v1/tours').get(getAllTours).post(creatTour);
 app.route('/api/v1/tours/:id').get(getTour).patch(updateTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(createUser);
+app
+  .route('/api/v1/users/:id')
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 //4. starts server
 const port = 3000;
