57858aec6b15a15bc98ec7cfe01cb325d1fbb317
Yue Zuo <woshizuoyue9201@yahoo.com>
2020-04-30 13:36:44 -0400

    add router.param

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,6 +4,17 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
 
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour id is: ${val}`);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
@@ -19,14 +30,6 @@ exports.getAllTours = (req, res) => {
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'Fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(200).json({
     status: 'sucess',
     data: {
@@ -55,13 +58,6 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
   res.status(200).json({
     status: 'success',
     data: {
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const tourController = require('../controllers/tourController');
 
+router.param('id', tourController.checkID);
+
 router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
 router
-  .route('./:id')
+  .route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour);
 
