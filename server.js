806df30eac860b1c05fb5f95032a265455088062
Yue Zuo <woshizuoyue9201@yahoo.com>
2020-05-04 12:33:14 -0400

    add database connections

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  //.connect(process.env.DATABASE_LOCAL, {
+    .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
 //console.log(process.env.PORT);
 const port = 3000 || process.env.PORT;
 app.listen(port, () => {
