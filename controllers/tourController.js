2d34061ac9557872f1208f4cb9d1ad65e95f6ff9
Yue Zuo <woshizuoyue9201@yahoo.com>
2020-05-18 11:58:39 -0400

    delete document

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -75,3 +75,18 @@ exports.updateTour = async (req, res) => {
     });
   }
 };
+
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -13,6 +13,7 @@ router
 router
   .route('/:id')
   .get(tourController.getTour)
-  .patch(tourController.updateTour);
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
