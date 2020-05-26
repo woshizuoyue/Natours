0d52714166fb72a873f6612ad8ca1ae038688b65
Yue Zuo <woshizuoyue9201@yahoo.com>
2020-05-26 11:11:28 -0400

    pipeline aggregate

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,6 +9,9 @@ const tourController = require('../controllers/tourController');
 router
   .route('/top-5-cheap')
   .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route('/tour-stats').get(tourController.getTourStats);
+
 router
   .route('/')
   .get(tourController.getAllTours)
